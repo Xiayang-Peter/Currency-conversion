c28197e82f843a3d008d38f052dc774934804cac
Xiayang-Peter <xiayang123wert@Outlook.com>
2021-12-20 20:32:40 +1300

    1

--- api-requests-test.py
+++ api-requests-test.py
@@ -6,11 +6,12 @@
 import requests
 url= 'https://v6.exchangerate-api.com/v6/c24594258bb5eec9b26a0950/pair/NZD/CNY' # api所在url
 response = requests.get(url)    # 从url获取并赋值data
-data = response.json()  # 从url获取并赋值data
+odata = response.json()  # 从url获取并赋值data
 # 输出api返回信息（保留）
 #print(data)
-data=str(data) #转换为string
+data=str(odata) #转换为string
 data=(data[391:397]) 
 data=float(data)
 print("当前比率为:",data)
+print(type(odata))
 
